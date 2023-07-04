9a92836271a7467df3f255b03060fa6030039365
yavuzselimgoc <202804008@ogr.cbu.edu.tr>
2023-07-04 08:42:39 +0300

    first-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PublicLayoutComponent } from './components/public-layout/public-layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PrivateLayoutComponent } from './components/private-layout/private-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: PublicLayoutComponent, children: [
+      {path:"",pathMatch:"full",component:NavbarComponent},
+  
+     
+    ]
+  },
+  {
+    path: "login", component: LoginComponent, children: [
+      { path: "", pathMatch: "full", component: LoginComponent },
+      { path: "login", component: LoginComponent },
+    ]
+  },
+  {
+    path: "admin", component: PrivateLayoutComponent, children: [
+     // { path: "", pathMatch: "full", component: AdminIndexComponent },
+ 
+    ]
+  }
+
+
+
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
+
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicLayoutComponent } from './components/public-layout/public-layout.component';
+import { PrivateLayoutComponent } from './components/private-layout/private-layout.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicLayoutComponent,
+    PrivateLayoutComponent,
+    LoginComponent,
+    NavbarComponent,
+    HeaderComponent,
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule, 
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
